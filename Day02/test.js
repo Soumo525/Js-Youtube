d9a5199d4f2e5e395a627dbcf733431a73eacadb
Soumo525 <128988403+Soumo525@users.noreply.github.com>
2023-07-13 06:17:21 +0000

    Let, Const and Var

---
+++ Day02/test.js
@@ -0,0 +1,15 @@
+const accountId = 525700
+let accountEmail = "abc@gamil.com"
+var accountPass = "123"
+accountCity = "Howrah" // not Prefer
+let accountState;
+// accountId = 999 // Not allowed
+accountEmail = "xyz@gamil.com"
+accountPass = "546"
+accountCity = "Nodia"
+ console.log(accountEmail);
+ console.table([accountId,accountEmail,accountPass,accountCity,accountState])// New tech
+/*
+Prefer not to use var
+because of issue in block scope and functional scope 
+*/
