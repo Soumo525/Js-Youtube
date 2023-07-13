cef69619337f966fff326e9309a4a0817fc7a39c
Soumo525 <128988403+Soumo525@users.noreply.github.com>
2023-07-13 06:58:41 +0000

    Datatypes and ECMA standards

---
+++ Day02/test1.js
@@ -0,0 +1,19 @@
+"use strict"; // treat all JS code as newer version
+
+// alert ( 3 + 3) // we are using nodejs, not browser
+
+// TC39 JavaScript or MDN 
+
+let name = "soumo" // string 
+let age = 60 // number => 2 to power 53
+let isLoggedIn = false // boolean true /false
+
+// null => standalone value 
+// undefined =>
+// symbol => unique
+
+//object
+
+console.log(typeof age); // number
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
