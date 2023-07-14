dc5022a94c96fbb40c1730532a0f2142381df821
Soumo525 <128988403+Soumo525@users.noreply.github.com>
2023-07-14 15:23:35 +0000

    Operations

---
+++ Day03/test02.js
@@ -0,0 +1,24 @@
+// Operations 
+let value = 3
+let negvalue = -value
+console.log(negvalue);
+
+let str1 = "Hello"
+let str2 = " Soumo"
+
+let str3 = str1 + str2 
+console.log(str3);
+
+console.log("1" + 2) // 12
+console.log(1 + "2") // 12
+console.log("1" + 2 + 2); // 122
+console.log(1 + 2 + "2"); // 32
+
+console.log(+true); // 1 // not rec
+console.log(+""); // 0 // not rec
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);// 101   
+
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
