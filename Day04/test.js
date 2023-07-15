a5d2d19a7b859d1c63d3e54489109e743ac2c8d8
Soumo525 <128988403+Soumo525@users.noreply.github.com>
2023-07-15 15:13:19 +0000

    Comparison of datatypes

---
+++ Day04/test.js
@@ -0,0 +1,22 @@
+// comparison
+
+// console.log("2">1); // different data type 
+
+// console.log(null > 0); Dose not Rec
+// console.log(null == 0);
+// console.log(null >= 0);
+// console.log(null <= 0);
+
+/* 
+Comparisons convert null to a number, treating it as 0
+that's why (3) point null>= is ture and (1) point null > 0
+is false
+*/
+
+
+//console.log(undefined == 0); // false 
+
+
+// === check datatype
+
+console.log("2" === 2); // false because of "2" is string and 2 is integer 
