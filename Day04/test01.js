974e3112ba6adeeea8e518c171fb8d776a572a46
Soumo525 <128988403+Soumo525@users.noreply.github.com>
2023-07-15 15:38:39 +0000

    Data types of javascript summary

---
+++ Day04/test01.js
@@ -0,0 +1,34 @@
+// Data types of javascript summary
+
+// Primitive call by value 
+// 7 types : String, Number, Boolean, Null, Undefined, Symbol, BigInt
+
+
+// Reference (Non Primitive)
+// Array, Object, Functions
+
+
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+ //console.log(id === anotherId);
+
+ const bigNumber = 56453554354545456654564564n
+
+
+ // Reference (Non Primitive)
+// Array, Object, Functions
+
+const heros = ["xyz","abc", "pqr"] // Array
+
+
+let myObj = {
+    name: "soumo",
+    age: 22,
+}
+
+const myFunction = function(){
+    console.log("Hello World");
+}
+
