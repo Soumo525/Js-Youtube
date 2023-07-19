3866825f1b8f15b00205c68104292d482d0cb37d
Soumo525 <128988403+Soumo525@users.noreply.github.com>
2023-07-19 13:50:09 +0000

    Number

---
+++ Day06/test01.js
@@ -0,0 +1,18 @@
+// Number and Math
+
+const score = 400
+console.log(score);
+
+const balance = new Number(500)
+console.log(balance)
+
+console.log(balance.toString().length)
+console.log(balance.toFixed(2));
+
+const otherNumber = 54.5645
+
+console.log(otherNumber.toPrecision(3));
+
+const abc = 100000
+console.log(abc.toLocaleString('en-IN'));
+
