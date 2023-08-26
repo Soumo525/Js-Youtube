f9feca2b47854399c38cf570d1b1088fd34b01bb
Soumo525 <128988403+Soumo525@users.noreply.github.com>
2023-08-26 11:08:40 +0000

    Object 3

---
+++ Day08/test03.js
@@ -0,0 +1,19 @@
+// Object 3
+
+const course = {
+    coursename : "ABC",
+    price :"99",
+    teacher : "Soumo"
+
+}
+
+const {teacher: aaa} = course // destructure 
+
+console.log(aaa);
+
+// json
+// {
+//     "name" : "abc",
+//     "coursenmae" : "aaa",
+//     "price" : "Free"
+// }
