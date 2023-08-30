6b3d18b7c70dfe6c2835daf2fd0a116b4cf8e18c
Soumo525 <128988403+Soumo525@users.noreply.github.com>
2023-08-30 05:56:45 +0000

    While and do while loop

---
+++ Day12/test01.js
@@ -0,0 +1,22 @@
+// While and do while loop 
+
+let x =4
+
+while (x!=10) {
+    //console.log("Yes");
+    x++;
+}
+
+let myArr = ["A", "B", "C", "D"]
+let arr = 0;
+while (arr < myArr.length) {
+    //console.log(`value is ${myArr[arr]}`);
+    arr++;
+}
+
+let a = 1
+
+do {
+    console.log(`value is ${a}`);
+    a++;
+} while (a <= 10);
