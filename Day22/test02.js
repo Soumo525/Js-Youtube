1bb395431740f3e7722953c2cc22cbc86002f5c8
Soumo525 <128988403+Soumo525@users.noreply.github.com>
2023-09-16 16:45:17 +0000

    Call and this

---
+++ Day22/test02.js
@@ -0,0 +1,17 @@
+// Call and this 
+
+
+function setUserName(username){
+    this.username = username
+}
+
+function createUser(username, email, password){
+    setUserName.call(this, username)
+
+    this.email = email
+    this.password = password
+}
+
+
+const chai = new createUser("AAA", "AA@", "123")
+console.log(chai);
